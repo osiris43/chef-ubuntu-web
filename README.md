1850262859f6f39e006fd81db9ce8c8408cdc4e5
Brett Bim <osiris43@gmail.com>
2016-09-22 10:49:28 -0500

    Add generated cookbook content

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # chef-ubuntu-web
+# awesome_customers_ubuntu
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: awesome_customers_ubuntu
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
